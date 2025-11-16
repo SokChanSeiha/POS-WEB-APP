48d0191b090103d161b28db74c5c5f8ba9d48401
SokChanSeiha <sokchanseiha@gmail.com>
2025-11-16 12:43:38 +0700

    improve UI log in

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+  loginHelpText: string = "Incorrect password or email.";
+  showAlert: boolean = false;
+  email: string = '';
+  password: string = '';
 
+  onSubmit() {
+    const isValid = this.validateCredentials(this.email, this.password);
+    
+    if (!isValid) {
+      this.loginHelpText = "Incorrect password or email.";
+      this.showAlert = true;
+    } else {
+      this.loginHelpText = "Login successful";
+      this.showAlert = true; // Show the success message
+    }
+  }
+
+  private validateCredentials(email: string, password: string): boolean {
+    return email === 'abc' && password === '123';
+  }
 }
